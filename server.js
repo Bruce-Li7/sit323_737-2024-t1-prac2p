3ab0a119ea8743152bfd6c3097cb8e74d5585e66
Bruce-Li7 <lijiaqi7788@gmail.com>
2024-03-18 19:31:22 +1100

    Built using Node.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var app = express();
 const PORT = process.env.PORT || 3000; // Define port which is on 3000
 // Define route to serve HTML page
 app.get('/', (req, res) => {
-    // Respond to user with html file.
-    res.sendFile(__dirname + '/index.html');
+    // Respond to user with heading title and short description.
+    res.send('<html><body><h1>Welcome to JiaqiLi\'s first website for SIT737</h1><h2>This is my first webstie. My Email address is ohg@deakin.edu.au. Welcome to contact me.</h2></body></html>');
 });
 
 // Start the server
